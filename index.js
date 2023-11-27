3ed187ff36a4636c42fdac72b063756103fc2849
BiswalAshu <aaronstone201299@gmail.com>
2023-11-27 18:09:31 +0530

    first test2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     console.log('Hello from Lambda!');
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambdaaaaa!'),
+        body: JSON.stringify('Hello from Ashutosh!'),
     };
     return response;
 };
